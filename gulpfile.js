479c6666179c7058a90bea3ae12e732892a2b783
Mitchell Gates <md.fragala@gmail.com>
2015-10-21 12:12:16 -0400

    Program servers, FTP.

--- gulpfile.js
+++ gulpfile.js
@@ -15,39 +15,26 @@ var data = require('gulp-data');
 var sass = require('gulp-sass');
 var argv = require('yargs').argv;
 var browser = require('browser-sync').create();
+var ftp = require('vinyl-ftp');
 
-var bsOpen = Boolean(argv.o);
-var bsTunnel = Boolean(argv.t);
-var bsAll = Boolean(argv.all);
-var preprocess = lazypipe()
-  .pipe(cssglob, {extensions: ['.scss']})
-  .pipe(cssimport, {extensions: ['scss']})
-  .pipe(sass);
+var $server = require('./server.json');
 
 gulp.task('default', ['serve']);
+gulp.task('build', ['pages', 'styles', 'scripts', 'images']);
 gulp.task('serve', function() {
-  browser.init({
-    server: './app/',
-    ghostMode: false,
-    tunnel: bsTunnel
-      ? 'bishopsplace'
-      : false,
-    open: bsTunnel
-      ? 'tunnel'
-      : (bsOpen
-        ? 'external'
-        : false
-      ),
-    browser: bsAll
-      ? ['google chrome','firefox','safari']
-      : 'google chrome',
-    notify: false,
-    injectChanges: true
-  });
-  gulp.watch(['./src/scss/*.scss'],'styles');
-  gulp.watch(['./src/assets/scripts/**/*.js'],'scripts');
-  gulp.watch(['./assets/img/*.{jpg,jpeg,png,svg}'],'images');
-  gulp.watch(['./src/**/*.html','./src/assets/data/**/*'],'pages');
+  if (ENV === 'development') {
+    browser.init(sync);
+    gulp.watch(['./src/scss/*.scss'], ['styles']);
+    gulp.watch(['./src/assets/scripts/**/*.js'], ['scripts']);
+    gulp.watch(['./assets/img/*.{jpg,jpeg,png,svg}'], ['images']);
+    gulp.watch(['./src/**/*.html','./src/assets/data/**/*'], ['pages']);
+  } else {
+    var conn = ftp.create(connection);
+    deploy(conn, ['./app/assets/css/**/*.css']);
+    deploy(conn, ['./app/assets/js/**/*.js']);
+    deploy(conn, ['./app/assets/img/**/*.img']);
+    deploy(conn, ['./app/**/*.html']);
+  }
 });
 gulp.task('styles', function() {
   return gulp.src('./src/scss/*.scss')
@@ -56,7 +43,8 @@ gulp.task('styles', function() {
     .pipe(preprocess())
     .pipe(cssmin())
     .pipe(souremaps.write())
-    .pipe(gulp.dest('./app/assets/css/'));
+    .pipe(gulp.dest('./app/assets/css/'))
+    .pipe(browser.stream({once:true}));
 });
 gulp.task('scripts', function() {
   return gulp.src('./src/assets/scripts/**/*.js')
@@ -64,13 +52,15 @@ gulp.task('scripts', function() {
   .pipe(souremaps.init())
   .pipe(uglify())
   .pipe(souremaps.write())
-  .pipe(gulp.dest('./app/assets/js/'));
+  .pipe(gulp.dest('./app/assets/js/'))
+  .pipe(browser.stream({once:true}));
 });
 gulp.task('images', function() {
   return gulp.src('./assets/img/*.{jpg,jpeg,png,svg}')
   .pipe(plumber())
   .pipe(imagemin())
-  .pipe(gulp.dest('./app/assets/img/'));
+  .pipe(gulp.dest('./app/assets/img/'))
+  .pipe(browser.stream({once:true}));
 });
 gulp.task('pages', function() {
   nunjucks(['src/partials/'], {watch: false});
@@ -80,9 +70,79 @@ gulp.task('pages', function() {
   .pipe(data(context))
   .pipe(render())
   .pipe(souremaps.write())
-  .pipe(gulp.dest('./app/'));
+  .pipe(gulp.dest('./app/'))
+  .pipe(browser.stream({once:true}));
 });
 
-function context(file) {
+/******************
+  FLAGS & HELPERS
+******************/
+/**
+ * Option Flags
+ * * dev - ENV development
+ * * stage - ENV staging
+ * * pro - ENV production
+ */
+var ENV = !!argv.dev
+  ? 'development'
+  : !!argv.stage
+    ? 'staging'
+    : !!argv.pro
+      ? 'production'
+      : 'development';
+var $_DEV = (ENV === 'development');
+/**
+ * Option Flags:
+ * * cross - open multiple browsers
+ * * dev - ENV development
+ * * stage - ENV staging
+ * * pro - ENV production
+ * * o - open browser(s)
+ * * t - tunnel server
+ */
+var sync = {
+  ghostMode: false,
+  notify: false,
+  injectChanges: true,
+  browser: !!argv.cross
+    ? ['google chrome','firefox','safari']
+    : 'google chrome',
+  open: !!argv.o
+    ? !!argv.t
+      ? 'tunnel'
+      : 'ui-external'
+    : false,
+  proxy: false,
+  port: 9000,
+  server: ENV === 'development'
+    ? './app/'
+    : false,
+  tunnel: !!argv.t
+    ? 'bishops-place'
+    : false
+};
+var connection = {
+  host: $_DEV ? '' : $server[ENV].host,
+  user: $_DEV ? '' : $server[ENV].username,
+  password: $_DEV ? '' : $server[ENV].password,
+  port: $_DEV ? '' : $server[ENV].port || 21
+}
+var context = function(file) {
   return reddir('./src/assets/data/');
 }
+/**
+ * Deploy files to server
+ * @param conn - instance of vinyl-ftp
+ * @param {array} src - source globs
+ * @param {string} dest - destination directory
+ */
+var deploy = function(conn, globs) {
+  console.log('Initiating deploy...');
+  gulp.src(globs, {buffer: false})
+    .pipe(conn.newer($server[ENV].path))
+    .pipe(conn.dest($server[ENV].path));
+}
+var preprocess = lazypipe()
+  .pipe(cssglob, {extensions: ['.scss']})
+  .pipe(cssimport, {extensions: ['scss']})
+  .pipe(sass);
