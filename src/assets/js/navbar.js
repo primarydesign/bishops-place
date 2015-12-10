7d8592d6aab8585b1fe63325603157c938d707bb
niko-matses <nikolas.matses@gmail.com>
2015-12-10 15:29:15 -0500

    mobile nav styled and functioning

--- src/assets/js/navbar.js
+++ src/assets/js/navbar.js
@@ -1,6 +1,6 @@
 (function(window, $, undefined){$(document).ready(function() {
 
-  var navlinks = $('.nav--link');
+  var navlinks = $('.nav--link, .mobile-nav--link');
 
   navlinks.on('click', function(event) {
     event.preventDefault();
