321becc6984fb6063841f29a58481067ca9eccd8
Mitchell Gates <md.fragala@gmail.com>
2015-12-10 14:42:17 -0500

    Add marker icons.

--- src/assets/js/neighborhood.js
+++ src/assets/js/neighborhood.js
@@ -10,7 +10,7 @@
     minZoom: 4,
     scrollwheel: false,
     streetViewControl: false,
-    zoom: 14,
+    zoom: 13,
     styles: [{"featureType":"landscape","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"poi","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"stylers":[{"hue":"#00aaff"},{"saturation":-100},{"gamma":2.15},{"lightness":12}]},{"featureType":"road","elementType":"labels.text.fill","stylers":[{"visibility":"on"},{"lightness":24}]},{"featureType":"road","elementType":"geometry","stylers":[{"lightness":57}]}]
   };
   var NeighborhoodMap = new Mapster('map-canvas', options, Attractions);
@@ -27,6 +27,7 @@
       });
     } else {
       NeighborhoodMap.eachMarker(function() {
+        this.infoWindow.close();
         if (this.categories.indexOf(category) > -1) {
           this.setVisible(true);
         } else if (this.categories.indexOf('home') < 0) {
