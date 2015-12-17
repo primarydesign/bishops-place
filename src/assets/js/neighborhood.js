f11059b89dc6a3e74d5ac880f3ac51c20216e9fe
Mitchell Gates <md.fragala@gmail.com>
2015-12-17 12:45:47 -0500

    Make interface update map center and marker visibility.

--- src/assets/js/neighborhood.js
+++ src/assets/js/neighborhood.js
@@ -1,11 +1,9 @@
 (function(window, $, undefined){$(document).ready(function() {
 
   var Attractions = window.__neighborhoodAttractions__;
+  var BishopsPlace = { lat: 41.786806, lng: -72.745384 };
   var options = {
-    center: {
-      lat: 41.786806,
-      lng: -72.745384
-    },
+    center: BishopsPlace,
     mapTypeControl: false,
     minZoom: 4,
     scrollwheel: false,
@@ -37,21 +35,24 @@
     }
   });
 
-  var category = '.mapInterface__category';
+  var categoryItem = '.mapInterface__category';
   var categoryLabel = '.category__label';
   var categoryName = '.category__name';
   var categoryMatches = '.category__matches';
   $('.category__label').on('click', function() {
     var self = $(this);
-    var translate = !self.hasClass('is-selected')
-      ? -(100/7) * self.closest(category).index()
-      : 0;
+    var index = self.closest(categoryItem).index();
+    var category = self.data('category');
+    /* reassign interface states */
     self.toggleState('selected', categoryLabel);
     self.find(categoryName).toggleState('highlighted', categoryName);
     self.siblings(categoryMatches).toggleState('open', categoryMatches);
-    self.closest(category).toggleState('selected', category);
+    self.closest(categoryItem).toggleState('selected', categoryItem);
+    /* manage interface animation */
     $('.mapInterface__categories').velocity({
-      translateY: translate + '%'
+      translateY: self.hasClass('is-selected')
+        ? (-(100/7) * index) + '%'
+        : 0
     },{
       duration: 300,
       complete: function(elements) {
@@ -62,6 +63,40 @@
             wheelStep: 10
           });
         }
+      }
+    });
+    /* update map markers */
+    NeighborhoodMap.center(BishopsPlace);
+    if (self.hasClass('is-selected')) {
+      NeighborhoodMap.eachMarker(function() {
+        this.infoWindow.close();
+        if (this.categories.indexOf(category) > -1) {
+          this.setVisible(true);
+        } else if (this.categories.indexOf('home') < 0) {
+          this.setVisible(false);
+        }
+      });
+    } else {
+      NeighborhoodMap.eachMarker(function() {
+        this.infoWindow.close();
+        this.setVisible(true);
+      });
+    }
+  });
+  $('.category__match').on('click', function() {
+    var match = $(this);
+    var name = match.data('name');
+    var latlng = match.attr('data-latlng').split(',');
+    NeighborhoodMap.center({
+      lat: parseFloat(latlng[0]),
+      lng: parseFloat(latlng[1])
+    });
+    NeighborhoodMap.eachMarker(function() {
+      this.infoWindow.close();
+      if (this.name === name) {
+        this.setVisible(true);
+      } else if (this.categories.indexOf('home') < 0) {
+        this.setVisible(false);
       }
     });
   });
