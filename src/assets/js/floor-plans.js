e7d655e2241866642add1d6e0c58de9734fc23b2
Mitchell Gates <md.fragala@gmail.com>
2016-02-24 13:41:02 -0500

    Add representative floor plans.

--- src/assets/js/floor-plans.js
+++ src/assets/js/floor-plans.js
@@ -22,7 +22,7 @@
   /* Open and Close the Floorplan Modal */
   $('.table--row.row_body').on('click', function() {
     var data = $(this).data();
-    $('.page').append(buildModal(data.br, data.ba, data.sqft));
+    $('.page').append(buildModal(data.pn, data.br, data.ba, data.sf));
     $('.floorplan-modal__close').on('click', function() {
       $('.floorplan-modal').remove();
       $('body').removeClass('is-locked');
@@ -40,7 +40,7 @@
    * Constructs a document fragment containing the floorplan modal to be inserted into the DOM
    * @return {document fragment} - the floorplan modal structure
    */
-  function buildModal(br, ba, sf) {
+  function buildModal(id, br, ba, sf) {
     var bedroomType, bathroomType;
     switch (br) {
       case 0: bedroomType = 'Studio'; break;
@@ -69,9 +69,9 @@
             '</li>' +
           '</ul>' +
           '<figure class="floorplan-modal__figure">' +
-            '<img src="assets/img/floorplans/' + br + 'br-' + ba + 'ba-' + sf + '.svg" alt="' + br + 'br | ' + ba + 'ba | ' + sf + ' sq.ft.">' +
+            '<img src="assets/img/plans/plan-' + id + '.svg" alt="' + br + 'br | ' + ba + 'ba | ' + sf + ' sq.ft.">' +
           '</figure>' +
-          '<a class="floorplan-modal__download" href="assets/plans/' + br + 'br_' + ba + 'ba_' + sf + '.pdf" target="_blank">Download PDF</a>' +
+          '<a class="floorplan-modal__download" href="assets/plans/plan-' + id + '.pdf" target="_blank">Download PDF</a>' +
           '<p class="floorplan-modal__disclaimer">These floor plans are intended for illustrative purposes only and are not to scale; square foot measurements and dimensions are approximate and configurations may vary from unit to unit.</p>' +
           '<a class="floorplan-modal__close">X</a>' +
           '<ul class="floorplan-modal__icons">' +
